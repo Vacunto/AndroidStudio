465fc52bd208dce6cd75d9a74511b733bc895cd7
Vacunto <100958837+Vacunto@users.noreply.github.com>
2022-05-08 21:36:40 +0300

    Lesson

---
+++ AndroidStudioProjects/Lesson_1/ClickButtons/src/main/java/com/mirea/levitis/clickbuttons/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mirea.levitis.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    protected void onCreate_1(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
